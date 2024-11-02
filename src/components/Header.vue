7ff8f6880d9195d0b0ca2881b4c2b15a61f1b8b8
666Cerber666 <forgikiname@mail.ru>
2024-11-02 22:21:52 +0400

    Закрепил хэдер

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="fixed-header">
     <div class="flex items-center justify-center h-full gap-6 flex-wrap px-2">
       <div class="nav_link px-2 text-center" @click="scrollToSection('what')">что это</div>
       <div class="nav_link px-2 text-center" @click="scrollToSection('partners')">маркетплейсы</div>
@@ -22,20 +22,28 @@ const scrollToSection = (sectionId) => {
 </script>
 
 <style scoped>
+.fixed-header {
+  position: fixed; /* Фиксируем заголовок */
+  top: 0; /* Располагаем его в верхней части экрана */
+  left: 0; /* Выравниваем по левому краю */
+  right: 0; /* Выравниваем по правому краю */
+  z-index: 1000; /* Устанавливаем высокий z-index, чтобы заголовок был над другими элементами */
+  background-color: black; /* Задаем фон, чтобы заголовок был виден */
+}
+
 .nav_link {
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   color: white;
-  background-color: black;
   font-family: Roboto;
   font-size: 18px; /* Стандартный размер шрифта */
   font-weight: 700;
   text-align: center;
   transition: color 0.3s ease, background-color 0.3s ease;
   min-width: 50px;
-  max-width:100%;
+  max-width: 100%;
 }
 
 .nav_link:hover {
